ba657acf0ea8d022587f71e291ce559ef994580a
Suyoung Jung <tdhitbtd@gmail.com>
2023-08-25 10:50:04 +0900

    로그인 유지(세션) & 로고

--- springboard/src/main/java/kr/ac/kopo/member/controller/MemberController.java
+++ springboard/src/main/java/kr/ac/kopo/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package kr.ac.kopo.member.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +11,33 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import kr.ac.kopo.member.service.MemberService;
 import kr.ac.kopo.member.vo.LoginVO;
 import kr.ac.kopo.member.vo.MemberVO;
 
+@SessionAttributes("currentUser")
 @Controller
 public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
 	
+	@GetMapping("/logout")
+	public String logout2(SessionStatus sessionStatus) {
+		sessionStatus.setComplete();
+		return "redirect:/";
+	}	
+	
+	//@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		//session.setAttribute("currentUser", session);
+		//session.removeAttribute("currentUser");
+		//session.invalidate();
+		return "redirect:/";
+	}
 	@GetMapping("/login")
 	public String loginForm(Model model) {
 		LoginVO loginVO = new LoginVO();
@@ -28,7 +46,11 @@ public class MemberController {
 	}
 	
 	@PostMapping("/login")
-	public String loginProcess(@Valid LoginVO loginVO, Errors errors, Model model) {
+	public String loginProcess(@Valid LoginVO loginVO, 
+								Errors errors, 
+								Model model,
+								HttpServletRequest request,
+								HttpSession session) {
 		
 		if(errors.hasErrors()) {	//error가 있는 경우
 			return "member/loginForm";	//다시 로그인하는 페이지로 이동
@@ -40,12 +62,15 @@ public class MemberController {
 				model.addAttribute("msg", "id와 password를 다시 확인하세요.");
 				return "member/loginForm";	//다시 로그인하는 페이지로 이동
 			} else {
-				System.out.println("post logi이 완료되었습니다.");
+				System.out.println("post login이 완료되었습니다.");
 				System.out.println(memberVO);
-				return "redirect:/board";
+				//session에 등록하기
+					//1) 첫번째 방법session.setAttribute("currentUser", memberVO);
+					//2) 두번째 방법 @SessionAttribures를 class명 위에 설정한다
+					model.addAttribute("currentUser", memberVO);
+				return "redirect:/";
 				//http://localhost:8080/springboard/board				
 			}
 		}
-
 	}
 }
---
+++ springboard/src/main/java/kr/ac/kopo/member/vo/JoinVO.java
@@ -0,0 +1,6 @@
+package kr.ac.kopo.member.vo;
+
+public class JoinVO {
+	
+	private String name
+}
